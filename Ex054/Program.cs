e2d80f9474bfe3005d3e8257b99abac9237b1b31
AnimewkaSes <brol0526@gmail.com>
2022-12-24 01:20:28 +0600

    ДЗ-8

---
+++ Ex054/Program.cs
@@ -0,0 +1,89 @@
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Введите количество столбцов в массиве: ");
+        int colSize = Int32.Parse(Console.ReadLine());
+        Console.Write("Введите количество строк в массиве: ");
+        int rowSize = Int32.Parse(Console.ReadLine());
+        int[,] mass = Array(rowSize,colSize);
+        Console.WriteLine("Исходный массив: ");
+        Print(mass);
+        
+        Console.WriteLine("Массив упорядочный по убыванию элементов каждой строки: ");
+        int[] row = new int[colSize];
+            for (int i = 0; i < rowSize; i++)
+            {
+                for (int j = 0; j < colSize; j++)
+                {
+                    row[j] = mass[i,j];
+               
+                }
+            BubbleSort(row);
+            Insert(true,i,row,mass);
+            }
+        Print(mass);
+
+       
+
+    
+    
+    }
+
+    public static int[,] Array(int r, int c){
+        int[,] arr = new int [r,c];
+        Random rnd = new Random();
+
+        for (int i = 0; i < r; i++)
+        {
+            for (int j = 0; j < c; j++)
+            {
+                 arr[i,j] = rnd.Next(0,9);
+            }   
+        }
+        return arr;
+    }
+
+    public static void Print(int[,] array){
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                Console.Write(array[i,j]+" ");
+            }
+            Console.WriteLine();
+        }
+
+    }
+
+    public static void BubbleSort(int[] array){
+        for (int i = 0; i < array.Length; i++)
+        {
+            for (int j = 0; j < array.Length-i-1; j++)
+            {
+                if (array[j]<array[j+1])
+                {
+                    int temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
+                }
+            }
+        }
+
+    }
+
+    public static void Insert(bool isRow, int dim, int[] sourse, int[,] dest){
+
+        for (int k = 0; k < sourse.Length; k++)
+        {
+            if (isRow)
+            {
+                dest[dim,k] = sourse[k];
+            } 
+          
+        }
+
+    }
+    
+}
---
+++ Ex056/Program.cs
@@ -0,0 +1,64 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Введите размер двумерного массива: ");
+        int size = Int32.Parse(Console.ReadLine());
+        int[,] array = Array(size,size);
+        PrintArray(array);
+        ColArray(array,size,size);
+      
+    }
+
+    public static int[,] Array(int r, int c){
+
+        int[,] table = new int[r,c];
+        Random rnd = new Random();
+        for (int i = 0; i < r; i++)
+        {
+            for (int j = 0; j < c; j++)
+            {
+                table[i,j] = rnd.Next(0,9);
+            }
+        }
+     return table;   
+    }
+    public static void PrintArray(int[,] arr){
+        for (int i = 0; i < arr.GetLength(0); i++)
+        {
+            for (int j = 0; j < arr.GetLength(1); j++)
+            {
+                Console.Write(arr[i,j]+" ");
+            }
+            Console.WriteLine();
+        }
+
+    }
+    public static void ColArray(int[,] mas,int r, int c ){
+        int minRowSum = int.MaxValue, indxeMinRow = 0;   
+        int sum = 0; 
+        for (int i = 0; i < r; i++)
+        {
+                       
+                for (int j = 0; j < c; j++)
+                {
+                    sum += mas[i,j];
+                    
+                 
+                 }
+
+            if (sum<minRowSum)
+                    {
+                        minRowSum = sum;
+                        indxeMinRow = i;
+                    }           
+            Console.WriteLine("Сумма элементов в строке {0} равно: {1}. ",i,sum);
+        }
+        Console.WriteLine("Минимальная строка c суммой элементов {0}", minRowSum);
+        for (int i = 0; i < r; i++)
+        {
+            Console.Write(mas[indxeMinRow,i]+" ");
+        }
+
+    }
+}
